bb8b1564cb0f6e031aa0df1dd295df62ec013561
rebornix <penn.lv@gmail.com>
2017-04-27 16:33:19 -0700

    Update ui part

--- index.ts
+++ index.ts
@@ -3,23 +3,43 @@ import { generateTheme, IColorSet } from 'vscode-theme-generator';
 const ansiGroups = require('nova-colors').ansiGroups
 
 const themeName = 'Nova';
-const colors = {
-	black: ansiGroups.normal.black,
-	white: ansiGroups.normal.white,
-	blue: ansiGroups.normal.blue,
-	red: ansiGroups.normal.red,
-	green: ansiGroups.normal.green,
-	yellow: ansiGroups.normal.yellow
-}
-
 const colorSet: IColorSet = {
 	base: {
-		background: colors.black,
-		foreground: colors.white,
-		color1: colors.blue,
-		color2: colors.red,
-		color3: colors.green,
-		color4: colors.yellow
+		background: ansiGroups.normal.black,
+		foreground: ansiGroups.normal.white,
+		color1: ansiGroups.normal.blue,
+		color2: ansiGroups.normal.red,
+		color3: ansiGroups.normal.green,
+		color4: ansiGroups.normal.yellow
+	},
+	ui: {
+		// 	/** The color of the editor cursor/caret */
+		// 	cursor: ,
+		// 	/** Visible whitespace (editor.renderWhitespace) */
+		// 	invisibles: ,
+		// 	/** Indent guide */
+		// 	guide: ,
+		/** Line highlight, this will remove the line borders in favor of a solid highlight */
+		lineHighlight: ansiGroups.normal.magenta,
+		// 	findMatchHighlight: ,
+		// 	currentFindMatchHighlight: ,
+		// 	findRangeHighlight: ,
+		// 	rangeHighlight: ,
+		// 	selectionHighlight: ,
+		selection: ansiGroups.normal.cyan,
+		// 	wordHighlight: ,
+		// 	wordHighlightStrong: ,
+		// 	activeLinkForeground: 
+	},
+	terminal: {
+		black: ansiGroups.normal.black,
+		red: ansiGroups.normal.red,
+		green: ansiGroups.normal.green,
+		yellow: ansiGroups.normal.yellow,
+		blue: ansiGroups.normal.blue,
+		magenta: ansiGroups.normal.magenta,
+		cyan: ansiGroups.normal.cyan,
+		white: ansiGroups.normal.white
 	}
 };
 
